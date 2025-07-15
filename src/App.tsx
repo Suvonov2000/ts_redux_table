2abdd28a6e5a0f3d252bf238e565f08167425c35
Suvonov2000 <suvonov200@gmail.com>
2025-07-15 11:37:25 +0900

    ts_redux_table

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import { useState, type FC } from "react"
 import { useAppDispatch, useAppSelector } from "./hooks/redux"
-import {add} from "./redux/tableSlice"
+import { add, deleteItem, setEditField } from "./redux/tableSlice"
 
-const App: FC =() =>{
+const App: FC = () => {
   const dispatch = useAppDispatch()
-  const {data} = useAppSelector(({table})=>table);
+  const { data } = useAppSelector(({ table }) => table);
   const [name, setName] = useState("");
   const [surname, setSurname] = useState("");
-  return(
+  const editField = useAppSelector(({ table }) => table.editField)
+
+  return (
     <div style={{
-      width:"100%",
-      height:"100%",
-      display:"flex",
-      flexDirection:"column",
-      alignItems:"center",
-      justifyContent:"cenetr",
+      width: "100%",
+      height: "100%",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "cenetr",
     }}>
-      <div style={{width:"100%", display:"flex", justifyContent:"center", }}>
-        <input type="text" placeholder="Name" value={name} onChange={(e)=>setName(e.target.value)} />
-        <input type="text" placeholder="Surname" value={surname} onChange={(e)=>setSurname(e.target.value)}/>
-        <button type="button" onClick={()=>{
-          dispatch(add({name, surname,}));
+      <div style={{ width: "100%", display: "flex", justifyContent: "center", }}>
+        <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
+        <input type="text" placeholder="Surname" value={surname} onChange={(e) => setSurname(e.target.value)} />
+        <button type="button" onClick={() => {
+          dispatch(add({ name, surname, }));
           setName("");
           setSurname("");
         }}>Add</button>
@@ -33,17 +35,52 @@ const App: FC =() =>{
           <th>Action</th>
         </thead>
         <tbody>
-          {data.map((item)=>(
-              <tr key={item.id}>
-                <td>{item.id}</td>
-                <td>{item.name}</td>
-                <td>{item.surname}</td>
-                <td>
-            <button type="button">Edit</button>
-            <button type="button">Delete</button>
-          </td>
-              </tr>
-            )) }
+          {data.map((item) => (
+            <tr key={item.id}>
+              <td>{item.id}</td>
+              <td>{editField?.id === item.id ? (
+                <input type="text" value={editField.name} onChange={(e) => dispatch(setEditField({ ...item, name: e.target.value }))} />
+              ) : (
+                item.name
+              )}</td>
+              <td>{editField?.id === item.id ? (
+                <input type="text" value={editField.surname} onChange={(e) => dispatch(setEditField({ ...item, surname: e.target.value }))} />
+              ) : (
+                item.surname
+              )}</td>
+              <td>
+                {
+                  editField?.id === item.id ? (
+                    <div>
+                      <button type="button" onClick={() => {
+
+                        dispatch(setEditField(null));
+                      }}
+                      >
+                        Cancel
+                      </button>
+                      <button type="button" onClick={() => {
+                        dispatch(save(editField))
+                        dispatch(setEditField(null));
+
+                      }}>
+                        Save
+                      </button>
+                    </div>
+
+                  ) : (
+                    <button
+                      type="button"
+                      onClick={() => setEditField(item)}>Edit</button>
+                  )
+                }
+
+                <button
+                  type="button"
+                  onClick={() => dispatch(deleteItem(item))}>Delete</button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
--- src/redux/tableSlice.tsx
+++ src/redux/tableSlice.tsx
@@ -1,27 +1,35 @@
-import {createSlice, type PayloadAction} from "@reduxjs/toolkit"
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit"
 import type { Todo } from "../type";
-import {v4} from "uuid";
+import { v4 } from "uuid";
 
 type TInitialState = {
-   data: Todo[];
+    data: Todo[];
+    editField: Todo | null;
 }
 
 const initialState: TInitialState = {
-    data:[],
+    data: [],
+    editField: null,
 }
 
 const tableSlice = createSlice({
-    name:"tableSlice",
+    name: "tableSlice",
     initialState,
-    reducers:{
-       add(state,{payload}: PayloadAction<Partial<Todo>>){
-        state.data.push({...(payload as Todo), id:v4()})
-       },
-       delete(state, {payload}: PayloadAction<Todo>) {
-        state.data = state.data.filter((item)=>item.id !== payload.id);
-       }
+    reducers: {
+        add(state, { payload }: PayloadAction<Partial<Todo>>) {
+            state.data.push({ ...(payload as Todo), id: v4() })
+        },
+        deleteItem(state, { payload }: PayloadAction<Todo>) {
+            state.data = state.data.filter((item) => item.id !== payload.id);
+        },
+        setEditField(state, { payload }: PayloadAction<Todo | null>) {
+            state.editField = payload;
+        },
+        save(state, { payload }: PayloadAction<Todo>) {
+            state.data = state.data.map((item) => item.id === payload.id ? payload : item)
+        }
     },
 })
 
-export const {add} = tableSlice.actions;
+export const { add, deleteItem, setEditField, save } = tableSlice.actions;
 export const tableReducer = tableSlice.reducer;
